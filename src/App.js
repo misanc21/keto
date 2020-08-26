41c4b2c26f83cfa906f978c0d1b57c53a9d7c9e3
misanc21 <misanc21@gmail.com>
2020-08-26 11:51:59 -0500

    estructura app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Aproximacion del punto medio</h1>
       </header>
+      <section className="contenido-principal contenido">
+        <div className="row">
+          <div className="one-half column">
+              datos
+          </div>
+          <div className="one-half column">
+              resultado
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
